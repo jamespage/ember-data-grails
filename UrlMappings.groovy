320cf2e8f2ec635c6d2c91a28520eba7aae1b33c
Zach Riggle <zach@riggle.me>
2013-03-23 15:38:03 -0400

    Remove unnecessary lines

--- UrlMappings.groovy
+++ UrlMappings.groovy
@@ -2,11 +2,9 @@ class UrlMappings {
   static mappings = {
     "/$controller"{
       action=[GET:"list", POST:"save"]
-      rest  =true
     }
     "/$controller/$id"{
       action=[GET:"show",  PUT:"update", DELETE:"delete"]
-      rest  =true
     }
   }
 }
